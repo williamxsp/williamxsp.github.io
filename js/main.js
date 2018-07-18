37a9504a2685e30da87824d022f963a41f944f2e
William  Martins <williamxsp@gmail.com>
2018-07-17 23:57:23 -0300

    Draft v1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    particlesJS.load('particles-js', 'assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+      });
+
+    //   let linksMenu = document.querySelectorAll("nav a");
+    //   linksMenu.forEach(a => {
+        // a.addEventListener("click", function(evt){
+            // let linkMenuAtivo = document.querySelector("nav .nav-item.active");
+            // linkMenuAtivo.classList.remove("active");
+            // evt.target.parentNode.classList.add("active");
+        // })
+    //   });
+
+    
+}
